fff6bb2021bdfa1967ec95ed8fd9a7268a6e985b
Chang Jang <changwoonjang@gmail.com>
2022-06-06 10:17:37 -0400

    Upload

---
+++ app.py
@@ -0,0 +1,192 @@
+# flask.exe run
+
+# Import Flask
+from flask import Flask, jsonify
+
+# Dependencies and Setup
+import numpy as np
+import datetime as dt
+
+# Python SQL Toolkit and Object Relational Mapper
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from sqlalchemy.pool import StaticPool
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# Reflect Existing Database Into a New Model
+Base = automap_base()
+# Reflect the Tables
+Base.prepare(engine, reflect=True)
+
+# Save References to Each Table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create Session (Link) From Python to the DB
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#https://portal.ehawaii.gov/assets/webp/page/government/counties/honolulu.webp
+#https://i.ytimg.com/vi/3ZiMvhIO-d4/maxresdefault.jpg
+#################################################
+# Flask Routes
+#################################################
+# Home Route
+@app.route("/")
+def welcome():
+        return """<html>
+<h1>Hawaii Climate App (Flask API)</h1>
+<img src="https://portal.ehawaii.gov/assets/webp/page/government/counties/honolulu.webp" alt="Hawaii Weather"/>
+<p>Precipitation Analysis:</p>
+<ul>
+  <li><a href="/api/v1.0/precipitation">/api/v1.0/precipitation</a></li>
+</ul>
+<p>Station Analysis:</p>
+<ul>
+  <li><a href="/api/v1.0/stations">/api/v1.0/stations</a></li>
+</ul>
+<p>Temperature Analysis:</p>
+<ul>
+  <li><a href="/api/v1.0/tobs">/api/v1.0/tobs</a></li>
+</ul>
+<p>Start Day Analysis:</p>
+<ul>
+  <li><a href="/api/v1.0/2017-03-14">/api/v1.0/2017-03-14</a></li>
+</ul>
+<p>Start & End Day Analysis:</p>
+<ul>
+  <li><a href="/api/v1.0/2017-03-14/2017-03-28">/api/v1.0/2017-03-14/2017-03-28</a></li>
+</ul>
+</html>
+"""
+
+# Precipitation Route
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all precipitation and date"""
+    # Query all precipitation and date
+    results = session.query(Measurement.date,Measurement.prcp).all()
+
+    session.close()
+
+    # Convert list of tuples into dictionary
+    all_precepitation=[]
+    for date,prcp in results:
+        precipitation_dict = {}
+        precipitation_dict[date] = prcp
+        all_precepitation.append(precipitation_dict)
+
+    return jsonify(all_precepitation)
+
+# Station Route
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all stations"""
+    # Query all stations
+    results = session.query(Station.id,Station.station,Station.name,Station.latitude,Station.longitude,Station.elevation).all()
+    session.close()
+    all_station=[]
+    for id,station,name,latitude,longitude,elevation in results:
+        station_dict={}
+        station_dict['Id']=id
+        station_dict['station']=station
+        station_dict['name']=name
+        station_dict['latitude']=latitude
+        station_dict['longitude']=longitude
+        station_dict['elevation']=elevation
+        all_station.append(station_dict)
+    return jsonify(all_station)
+
+# TOBs Route
+@app.route("/api/v1.0/tobs")
+def tempartureobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all temparture observation"""
+    # Calculate the date 1 year ago from the last data point in the database
+    results_date=session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    str_date=list(np.ravel(results_date))[0]
+    latest_date=dt.datetime.strptime(str_date,"%Y-%m-%d")
+    year_back=latest_date-dt.timedelta(days=366)
+# Perform a query to retrieve the data and precipitation scores
+    results=session.query(Measurement.date, Measurement.tobs).order_by(Measurement.date.desc()).\
+            filter(Measurement.date>=year_back).all()
+    session.close()
+    all_temperature=[]
+    for date,tobs in results:
+        tobs_dict={}
+        tobs_dict['date']=date
+        tobs_dict['tobs']=tobs
+        all_temperature.append(tobs_dict)
+    return jsonify(all_temperature)
+
+
+# Start Day Route
+@app.route("/api/v1.0/<start>")
+def start_day(start):
+     # Create our session (link) from Python to the DB
+    session = Session(engine)
+    """TMIN, TAVG, and TMAX for a list of dates.
+    
+    Args:
+        start_date (string): A date string in the format %Y-%m-%d
+        
+    Returns:
+        TMIN, TAVE, and TMAX
+    """
+    
+    results=session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+                filter(Measurement.date >= start).all()
+    session.close()
+    temp_obs={}
+    temp_obs["Min_Temp"]=results[0][0]
+    temp_obs["Avg_Temp"]=results[0][1]
+    temp_obs["Max_Temp"]=results[0][2]
+    return jsonify(temp_obs)
+
+
+# Start-End Day Route
+@app.route("/api/v1.0/<start>/<end>")
+def start_end_day(start, end):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    """TMIN, TAVG, and TMAX for a list of dates.
+    
+    Args:
+        start_date (string): A date string in the format %Y-%m-%d
+        end_date (string): A date string in the format %Y-%m-%d
+        
+    Returns:
+        TMIN, TAVE, and TMAX
+    """
+    
+    results=session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+                filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    session.close()
+    temp_obs={}
+    temp_obs["Min_Temp"]=results[0][0]
+    temp_obs["Avg_Temp"]=results[0][1]
+    temp_obs["Max_Temp"]=results[0][2]
+    return jsonify(temp_obs)
+
+
+# Define Main Behavior
+if __name__ == '__main__':
+    app.run(debug=True)
